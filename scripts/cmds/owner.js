636248b1e1d1456e538628d95d02d0f92f8eaf88
Amit Max <maxamit279@gmail.com>
2025-03-21 01:10:57 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -6,8 +6,8 @@ const path = require('path');
 module.exports = {
 	config: {
 		name: "owner",
-		aliases: ["info","inf"],
-		author: "Amit Max",
+		aliases: ["info"],
+		author: "Rexy",
 		role: 0,
 		shortDescription: " ",
 		longDescription: "",
@@ -18,18 +18,16 @@ module.exports = {
 	onStart: async function ({ api, event }) {
 		try {
 			const ownerInfo = {
-				name: 'ᴀᴍɪᴛ ᴍᴀx ',
-				class: 'ɪɴᴛᴇʀ 1ꜱᴛ ',
-				group: 'ᴀᴄᴄᴏᴜɴᴛɪɴɢ',
-				gender: 'ᴍᴀʟᴇ',
+				name: 'Amit Max  ',
+				class: 'Inter 1st ',
+				group: 'Accounting',
+				gender: 'Male',
 				Birthday: '18-05-2006',
-				religion: 'ʜɪɴᴅᴜ',
-				hobby: 'ꜰʟɪʀᴛɪɴɢ😗',
-				Fb: 'https://www.facebook.com/share/1FV6eJ1iY1/',
-				Relationship: 'ꜱɪɴɢʟᴇ ',
-				Height: '5.6',
-				Address: 'ᴋʜᴏᴋꜱʜ , ᴋᴜꜱʜᴛɪᴀ',
-				Instagram: 'amitmax44',
+				religion: 'Hindu',
+				hobby: 'Flirting 🫣',
+				Fb: 'https://www.facebook.com/share/12EXrb9PoVB/',
+				Relationship: 'Single ',
+				Height: '5"6'
 			};
 
 			const bold = 'https://imgur.com/a/UFN9esj.mp4';
@@ -45,19 +43,17 @@ module.exports = {
 			fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
 
 			const response = `
-𓀬 ᴏᴡɴᴇʀ ɪɴꜰᴏ 𓀬 \n
- ~ɴᴀᴍᴇ: ${ownerInfo.name}
- ~ᴄʟᴀꜱꜱ: ${ownerInfo.class}
- ~ɢʀᴏᴜᴘ: ${ownerInfo.group}
- ~ɢᴇɴᴅᴇʀ: ${ownerInfo.gender}
- ~ʙɪʀᴛʜᴅᴀʏ: ${ownerInfo.Birthday}
- ~ʀᴇʟɪɢɪᴏɴ: ${ownerInfo.religion}
- ~ʀᴇʟᴀᴛɪᴏɴꜱʜɪᴘ: ${ownerInfo.Relationship}
- ~ʜᴏʙʙʏ: ${ownerInfo.hobby}
- ~ʜᴇɪɢʜᴛ: ${ownerInfo.Height}
- ~ꜰʙ: ${ownerInfo.Fb}
- ~ᴀᴅᴅʀᴇꜱꜱ: ${ownerInfo.Address}
- ~ɪɴꜱᴛᴀɢʀᴀᴍ: ${OwnerInfo.Instagram}
+𓀬 𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎 𓀬 \n
+ ~𝙉𝘼𝙈𝙀: ${ownerInfo.name}
+ ~𝘾𝙇𝘼𝙎𝙎: ${ownerInfo.class}
+ ~𝙂𝙍𝙊𝙐𝙋: ${ownerInfo.group}
+ ~𝙂𝙀𝙉𝘿𝙀𝙍: ${ownerInfo.gender}
+ ~𝘽𝙄𝙍𝙏𝙃𝘿𝘼𝙔: ${ownerInfo.Birthday}
+ ~𝙍𝙀𝙇𝙄𝙂𝙄𝙊𝙉: ${ownerInfo.religion}
+ ~𝙍𝙀𝙇𝘼𝙏𝙄𝙊𝙉𝙎𝙃𝙄𝙋: ${ownerInfo.Relationship}
+ ~𝙃𝙊𝘽𝘽𝙔: ${ownerInfo.hobby}
+ ~𝙃𝙀𝙄𝙂𝙃𝙏: ${ownerInfo.Height}
+ ~𝙁𝘽: ${ownerInfo.Fb}
 			`;
 
 			await api.sendMessage({
@@ -76,4 +72,4 @@ module.exports = {
 };
 
 const wrapper = new GoatWrapper(module.exports);
-		wrapper.applyNoPrefix({ allowPrefix: true });
+wrapper.applyNoPrefix({ allowPrefix: true });
