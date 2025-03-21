3f68a75eb312fa90bd0c1fc7aba05ef6caee58ed
Amit Max <maxamit279@gmail.com>
2025-03-21 09:46:35 +0600

    Create tiktok video.js

---
+++ scripts/cmds/tiktok video.js
@@ -0,0 +1,21 @@
+{
+  "code": {
+    "error": "00000",
+    "msg": "接口调用成功"
+  },
+  "data": {
+    "sourceKey": "parse:url_cfa83d86b0d26677c98fe6a0d50f62ed",
+    "info": {
+      "title": "камин (tiktok version/sped up) - emin feat. jony [edit audio] full version",
+      "thumbnail": "https://i.ytimg.com/vi/2K6neRke3Ac/mqdefault.jpg",
+      "description": "🎵: камин (tiktok version/sped up) - emin feat. jony\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━  \n🌐 Socials\nInstagram: [@dhil.o](https://www.instagram.com/dhil.o)  \n\n📧 Copyright Info  \n- I do not own the rights to the anime images or background music featured in this video.  \n- All images and music are used under fair use guidelines.  \n- If there’s an issue with your content being used, please contact me at fadhil.oktafianto@gmail.com, and I’ll remove the video within 24 hours.  \n- No copyright infringement is intended.  \n- If you use my audio, kindly credit @prodbyteio.  \n\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━  \n💻 Editing Tools  \n- Audio : Djay Pro, FL Studio  24\n- Background: Picsart  \n- Visualizer/Spectrum: Avee Music Player  \n\n📱 Devices Used  \n- Audio: lenovo legion\n- Visualizer: Samsung Tab S7  \nAnime : beyond the boundary\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━  \nTags \n#editaudiobadass #tiktok #editaudio #tiktoktrend #камин #tiktokaudios #spedup  \n\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━  \n♡ 2025 TEIO",
+      "duration": "02:13",
+      "view_count": "461K",
+      "publishedAt": "2 months ago",
+      "subscribers": "7.14K",
+      "likes": "",
+      "player": "<iframe width=\"100%\" height=\"100%\" frameborder=\"0\" allowfullscreen src=\"https://www.amoyshare.com/player/?v=2K6neRke3Ac&watch=1742524648\"></iframe>",
+      "url": "https://youtu.be/2K6neRke3Ac?si=TN9JuEzQX_pvYpQU"
+    }
+  }
+}
